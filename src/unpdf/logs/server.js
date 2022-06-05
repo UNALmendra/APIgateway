f4fadb531693075444753df9003cd54b39ccf0a9
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2022-06-05 12:32:42 -0500

    tried to adjust docker-compose for docker containers

--- src/unpdf/logs/server.js
+++ src/unpdf/logs/server.js
@@ -1,3 +1,3 @@
-export const url = 'localhost'
+export const url = 'host.docker.internal'
 export const port = '9155'
 export const entryPoint = 'api/Logs'
--- src/unpdf/users/server.js
+++ src/unpdf/users/server.js
@@ -1,3 +1,3 @@
 export const url = 'localhost'
-export const port = '8080'
+export const port = '6868'
 export const entryPoint = 'users'
