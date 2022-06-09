4a4dab4c07cd3f3e74273bb72dcc703f20be55aa
damorenor <damorenor@unal.edu.co>
2022-06-09 01:27:25 +0000

    gcp stashed changes

--- src/unpdf/documents/server.js
+++ src/unpdf/documents/server.js
@@ -1,3 +1,3 @@
-export const url = 'localhost'
-export const port = '3001'
+export const url = '172.18.0.3'
+export const port = '3000'
 export const entryPoint = ''
--- src/unpdf/logs/server.js
+++ src/unpdf/logs/server.js
@@ -1,3 +1,3 @@
-export const url = 'host.docker.internal'
+export const url = 'localhost'
 export const port = '9155'
 export const entryPoint = 'api/Logs'
